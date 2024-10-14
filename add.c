0ec0b9ba776c4613d168b7e3a2bc3ed8b4e0e984
Samir Aryal <samiraryal@pop-os.localdomain>
2024-10-14 09:48:49 +0545

    C program

---
+++ add.c
@@ -0,0 +1,15 @@
+
+
+#include<stdio.h>
+void main()
+{
+    
+    int i=1,p;
+    do
+    {
+        p=5*i;
+        printf("\n5 *%d =%d ",i,p);
+        i++;
+    } while (i<=10);
+    
+}
---
+++ main.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+int main(    )
+{
+    int a,b ;
+    
+    a = 1;
+    b = 0;
+    printf("a&&b=%d\n" , a&&b);
+ printf("a-b=%d\n" , a-b);
+ printf("a*b=%d\n" , a*b);
+ printf("a/b=%d\n" , a/b);
+
+
+
+    return 0;
+}
+
+#include<stdio.h>
+void main()
+{
+    
+    int i=1,p;
+    do
+    {
+        p=5*i;
+        printf("\n5 *%d =%d ",i,p);
+        i++;
+    } while (i<=10);
+    
+}
