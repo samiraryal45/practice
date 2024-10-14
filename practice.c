6ffd0a37e4bd14ae47dd8797044de9e4564d2ebb
Samir Aryal <samiraryal24@gmail.com>
2024-10-14 15:18:38 +0545

    extra codes

--- practice.c
+++ practice.c
@@ -10,3 +10,19 @@ void main()
         i++;
     } while (i<=10);
     }
+#include<stdio.h>
+int main(    )
+{
+    int a,b ;
+    
+    a = 1;
+    b = 0;
+    printf("a&&b=%d\n" , a&&b);
+ printf("a-b=%d\n" , a-b);
+ printf("a*b=%d\n" , a*b);
+ printf("a/b=%d\n" , a/b);
+
+
+
+    return 0;
+}
