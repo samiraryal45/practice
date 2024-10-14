921ff3ed86b75c8497b6e9548b0411fd3009c66e
Samir Aryal <samiraryal24@gmail.com>
2024-10-14 10:23:14 +0545

    first commit

--- main.c
+++ main.c
@@ -15,17 +15,3 @@ int main(    )
 
     return 0;
 }
-
-#include<stdio.h>
-void main()
-{
-    
-    int i=1,p;
-    do
-    {
-        p=5*i;
-        printf("\n5 *%d =%d ",i,p);
-        i++;
-    } while (i<=10);
-    
-}
--- practice.c
+++ practice.c
@@ -2,11 +2,11 @@
 #include<stdio.h>
 void main()
 {
-    
     int i=1,p;
     do
     {
         p=5*i;
         printf("\n5 *%d =%d ",i,p);
         i++;
-    } while (i<=10);}
+    } while (i<=10);
+    }
