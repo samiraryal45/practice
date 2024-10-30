bfc09c155072352204251fd8173292eb18ec6b96
Samir Aryal <samiraryal24@gmail.com>
2024-10-30 13:26:06 +0545

    codes

--- extra.c
+++ extra.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 
+
 int main()
 {
-int x,y;
-printf("enter the value of x:\n");
-scanf("%d",&x);
-printf("enter the value of y:\n");
-scanf("%d",&y);
-if (x<y)
-{
-  printf("x is less than y");
-
-}
-else{
-  printf("y is less than x");
-}
-
+  int i;
+    while ( i)
+    {
+      printf("*");
+    }
+    
     return 0;
 }
--- practice.c
+++ practice.c
@@ -1457,3 +1457,16 @@ int main()
     
     return 0;}
 
+#include <stdio.h>
+
+int main()
+{
+    float x, y;
+    printf("Enter the value of x:\n");
+    scanf("%f", &x);
+    printf("Enter the value of y:\n");
+    scanf("%f", &y);
+    double z = (double)x / (double)y;
+    printf(" x divided by y is %.20f\n", z);
+    return 0;
+}
