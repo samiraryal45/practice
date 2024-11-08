8bed3f4713543479d8f6a3ce73927aed0c1ddfc7
Samir Aryal <samiraryal24@gmail.com>
2024-11-08 20:39:37 +0545

    codes

--- practice.c
+++ practice.c
@@ -2405,3 +2405,33 @@ int main() {
 
     return 0;
 }
+#include <stdio.h>
+
+void neon(int n){
+    int square = n*n;
+    int digit,sum=0;
+    while (square !=0)
+    {
+        digit =square %10;
+        sum +=digit;
+        square /=10;
+    }
+    if (sum==n)
+    {
+        printf("%d is a neon number.\n",n);
+    }
+    else{
+        printf("%d is not a neon number.\n",n);
+
+    }
+    
+    
+}
+int main()
+{
+    int n;
+    printf("enter a number:\n");
+    scanf("%d",&n);
+    neon(n);
+    return 0;
+}
