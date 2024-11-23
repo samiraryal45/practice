43a21b1058866f9ea1970b9823c6eda6f13b9d31
Samir Aryal <samiraryal24@gmail.com>
2024-11-23 20:44:38 +0545

    coding

--- extra.c
+++ extra.c
@@ -1,31 +1,20 @@
 #include <stdio.h>
 
+
 int main()
 {
-    int n, i;
-    printf("enter the no. of elements in an array.\n");
-    scanf("%d", &n);
-    int a[n];
-    printf("enter the elements in the array.\n");
-    for (i = 0; i < n; i++)
+    int n;
+    printf("enter the no. of lines.\n");
+    scanf("%d",&n);
+    for (int i = 0; i <n; i++)
     {
-        scanf("%d", &a[i]);
+       for (int j = 0; j <= i; j++)
+       {
+       printf("*");
+       }
+       printf("\n");
+       
     }
-    printf("The elements in the array are:\n");
-
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
-    }
-    printf("The negative elements in the array are:\n");
-    for (i = 0; i < n; i++)
-    {
-
-        if (a[i] < 0)
-        {
-            printf("%d ", a[i]);
-        }
-    }
-
+    
     return 0;
 }
